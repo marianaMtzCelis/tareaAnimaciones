bc8503eeaf79723feae246be75fa036dff04772c
diegomezcota <gomezcotadiego@gmail.com>
2020-03-24 16:31:40 -0700

    added save and load functions to game

--- VideoGame/src/videogame/Game.java
+++ VideoGame/src/videogame/Game.java
@@ -38,7 +38,6 @@ public class Game implements Runnable {
     private int score = 0;
     private LinkedList<GoodGuy> listaBuenos; // to use a list of enemies
     private boolean isPaused; // to pause or unpause game
-    private ReadandWrite progressFile; // class to save and load progress
     
     /**
      * to create title, width and height and set the game is still not running
@@ -56,7 +55,36 @@ public class Game implements Runnable {
         vidas = (int) (Math.random() * 3) + 3;
         counterVidas = 0;
         isPaused = false;
-        progressFile()
+    }
+    
+    public static void Save(String strFileName) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(strFileName));
+            int vidas = 5;
+            int score = 20;
+            writer.println("" + vidas + "/" + score);
+            writer.close();
+        } catch (IOException ioe) {
+            System.out.println("File Not found CALL 911");
+        }
+
+    }
+    
+    public static void Load(String strFileName) {
+        try {
+            FileReader file = new FileReader(strFileName);
+            BufferedReader reader = new BufferedReader(file);
+            String line;
+            String datos[];
+            line = reader.readLine();
+            datos = line.split("/");
+            int vidas = Integer.parseInt(datos[0]);
+            int score = Integer.parseInt(datos[1]);
+            System.out.println("Se leyo  vidas = "+ vidas + " y score = " + score);
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("File Not found CALL 911");
+        }
     }
 
     /**
